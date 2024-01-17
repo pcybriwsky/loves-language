8995f5ec0148f8e0bc68008ab49b10cd17a7cc3d
pcybriwsky <pcybriwsky@gmail.com>
2024-01-17 13:21:28 -0500

    File upload and working

---
+++ inkLine.js
@@ -0,0 +1,428 @@
+class InkLine {
+  constructor(colors, weight) {
+    this.colors = colors;
+    this.weight = weight;
+    this.drawFunction = null;
+    this.points = null;
+    this.pointsArray = null;
+    this.analogueness = false;
+    this.endBubbleP = 0;
+    this.splatterStart = 0;
+    this.splatterP = 0;
+    this.splats = 0;
+    this.addStops = 0;
+    this.shadeDirections = []
+    this.shade = 0;
+    this.fillType = null;
+    this.maxWeight = 0;
+  }
+
+  setAnalogueness(splitP, displacement) {
+    this.splitP = splitP;
+    this.displacement = displacement;
+  }
+
+  setEndBubble(bubbleP) {
+    this.endBubbleP = bubbleP;
+  }
+
+  setSplatter(minSplatterDistance, splatterP, maxSplats) {
+    this.splatterStart = minSplatterDistance;
+    this.splatterP = splatterP
+    this.maxSplats = maxSplats
+  }
+  
+  setWeight(weight){
+    this.maxWeight = weight  
+  }
+  
+  setDrawFunction(f){
+    this.drawFunction = f;
+  }
+  
+  setStops(n){
+    this.addStops = n;
+  }
+  
+  setShade(shade, direction) {
+    this.shade = 1;
+    this.shadeDirections.push(direction)
+  }
+  
+  setType(fillType){
+    this.fillType = fillType;
+  }
+  
+  setPoints(pointsArray){
+    this.points = pointsArray.length;
+    this.pointsArray = pointsArray
+  }
+  
+  setPointsFunction(x1, y1, x2, y2, numPoints){ 
+    this.points = this.drawFunction.formula(x1,y1, x2, y2, numPoints);
+  }
+
+  drawLine(x1, y1, x2, y2, count) {
+    ellipseMode(CENTER);
+    let totalColors = this.colors.length;
+    let strokeLength = 5000
+    let points = this.points
+    if(this.points == null){
+      let pointsArray = this.drawFunction.formula(x1,y1, x2, y2, points);
+      this.points = pointsArray;
+    }
+    
+    let displace = 1 - this.splitP;
+    
+    let endBubbleProb = this.endBubbleP; // // turn into a var with setter
+    let bubbleCheck = (Math.random(1) < endBubbleProb);
+    let splatterProb = this.splatterP; // turn into a var with setter
+    let willSplatter = (Math.random(1) < splatterProb); // turn into a var based on platter Prob and some random calc
+
+    let splatterDistance = points * this.splatterStart; // turn into a var with setter
+
+    // console.log(this.splats)
+    let splats = Math.ceil(random(1, this.maxSplats)); // turn into a var with setter
+    
+    
+    let weightF = (n, e) => {
+      let weight = map(n, 0, 1, 0, this.maxWeight)
+      return ((2 * weight + random(strokeLength)) / width) * e;
+    };
+
+    let displaceF = (n, d) => {
+      return noise(n) * d;
+    };
+
+    let c1 = color(this.colors[0][0], this.colors[0][1], this.colors[0][2]);
+    let c2 = color(this.colors[1][0], this.colors[1][1], this.colors[1][2]);
+
+    let showStops = this.addStops;
+    // console.log(c1, c2)
+    
+    let shade = 1;
+    // this.shade = 1;
+    
+    console.log(this.pointsArray, points)
+
+    for (let n = 0; n < points; n += 1) {
+      if(n < count){
+        let check = Math.random(1);
+        
+        let plotPoint = this.pointsArray[n]
+        let plotX = plotPoint.x
+        let plotY = plotPoint.y
+        
+        let inter = map(n, 0, points, 0, 1);
+        let c = lerpColor(c1, c2, inter);
+        fill(c);
+        noStroke()
+
+        if (willSplatter && n > splatterDistance) {
+          let totalPointsLeft = points - splatterDistance;
+          let cuts = Math.floor(totalPointsLeft / splats);
+          if (n%cuts == 0) {
+
+            let displacement = displaceF(n, this.displacement);
+            let weight = weightF(inter, 0.2);
+
+            ellipse(
+              plotX + displacement / 2,
+              plotY + displacement / 2,
+              weight,
+              weight
+            );
+            
+
+            if(this.shade){
+              stroke(c)
+              strokeWeight(weight)
+              let shade = map(n, 0, points, 0, 10)
+              line(plotX + shade, plotY + shade, plotX, plotY)
+            }
+          }
+        }
+        else if (check > this.splitP) {
+          let weight = weightF(inter, 0.15);
+          let displacement = displaceF(n, this.displacement);
+          ellipse(
+            plotX + displacement / 2,
+            plotY + displacement / 2,
+            weight,
+            weight
+          );
+          
+        }
+        if (bubbleCheck && n == points - 1 && !willSplatter) {
+          let weight = weightF(inter, 0.35);
+          ellipse(plotX, plotY, weight, weight);
+          // rect(
+          //     plotX + displacement / 2,
+          //     plotY + displacement / 2,
+          //     10,
+          //     1
+          //   );
+        }
+
+        if(showStops != 0 && n == 0){
+          let weight = weightF(inter, 0.35);
+          ellipse(plotX, plotY, weight, weight);
+          textAlign(CENTER, TOP)
+          if(this.drawFunction.title == 'line' || this.drawFunction.title == 'wave'){
+            if(x1 == x2 && y1 < y2){
+              text("a", plotX, plotY - 20)
+            }
+            
+            else if(y1 == y2 && x2 > x1){
+              text("a", plotX - 20, plotY)
+            }
+            
+            else if(y1 == y2 && x2 < x1){
+              text("a", plotX + 20, plotY)
+            }
+            
+            else{
+              text("a", plotX, plotY + 10)
+            }
+            
+            
+            
+          }
+          else{
+            text("a", plotX, plotY + 10)
+          }
+        }
+
+        if(showStops != 0 && n == points - 1){
+          let weight = weightF(inter, 0.35);
+          ellipse(plotX, plotY, weight, weight);
+          textAlign(CENTER, TOP)
+          if (this.drawFunction.title == 'circle' ){
+            text("b", plotX, plotY + 30)  
+          }
+          else if (this.drawFunction.title == 'spiral'){
+            text("b", plotX, plotY - 25)  
+          }
+          else if(this.drawFunction.title == 'line' || this.drawFunction.title == 'wave'){
+            // console.log("We're in line") 
+            if(x1 == x2){
+               if(y2 < y1){
+                 text("b", plotX, plotY - 20) 
+               }
+              
+               else{
+                 text("b", plotX, plotY + 10) 
+               }
+             }
+            
+            else if( y1 == y2){
+              if(x1>x2){
+                text("b", plotX - 20, plotY) 
+              }
+              else{
+                text("b", plotX + 20, plotY) 
+              }
+            }
+            
+            else if (y2 > y1){
+              text("b", plotX, plotY - 10) 
+            }
+          }
+          else{
+            text("b", plotX, plotY + 10)  
+          }
+          
+          // noLoop();
+        }
+      }
+    }
+  }
+  
+  animateLine(x1, y1, x2, y2, countStart, countEnd) {
+    ellipseMode(CENTER);
+    let totalColors = this.colors.length;
+    let strokeLength = 1
+    let points = this.points
+    if(this.points == null){
+      let pointsArray = this.drawFunction.formula(x1,y1, x2, y2, points);
+      this.points = pointsArray;
+    }
+    
+    let displace = 1 - this.splitP;
+    
+    let endBubbleProb = this.endBubbleP; // // turn into a var with setter
+    let bubbleCheck = (Math.random(1) < endBubbleProb);
+    let splatterProb = this.splatterP; // turn into a var with setter
+    let willSplatter = (Math.random(1) < splatterProb); // turn into a var based on platter Prob and some random calc
+
+    let splatterDistance = points * this.splatterStart; // turn into a var with setter
+
+    // console.log(this.splats)
+    let splats = Math.ceil(random(1, this.maxSplats)); // turn into a var with setter
+    
+    
+    let weightF = (n, e) => {
+      let weight = map(n, 0, 1, 0, this.maxWeight)
+      return ((2 * weight + random(strokeLength)) / width) * e;
+    };
+
+    let displaceF = (n, d) => {
+      return noise(n) * d;
+    };
+
+    let c1 = color(this.colors[0][0], this.colors[0][1], this.colors[0][2]);
+    let c2 = color(this.colors[1][0], this.colors[1][1], this.colors[1][2]);
+
+    let showStops = this.addStops;
+    
+    let shade = 1;
+    
+  // console.log(this.pointsArray)
+    beginShape();
+    for (let n = countStart; n < countEnd; n += 1) {
+      if(n < points){
+        let check = Math.random(1);
+        let plotPoint = this.pointsArray[n]
+        // console.log(plotPoint)
+        let plotX = plotPoint.x
+        let plotY = plotPoint.y
+        
+        let inter = map(n, 0, points, 0, 1);
+        let c = lerpColor(c1, c2, inter);
+        fill(c);
+        noStroke()
+        strokeWeight(1)
+        stroke(c);
+        weight = 10;
+
+        if (willSplatter && n > splatterDistance) {
+          let totalPointsLeft = points - splatterDistance;
+          let cuts = Math.floor(totalPointsLeft / splats);
+          if (n%cuts == 0) {
+
+            let displacement = displaceF(n, this.displacement);
+            let weight = weightF(inter, 0.2);
+            vertex(plotX + displacement / 2, plotY + displacement / 2)
+            ellipse(
+              plotX + displacement / 2,
+              plotY + displacement / 2,
+              weight,
+              weight
+            );
+
+            if(this.shade){
+              stroke(c)
+              strokeWeight(weight)
+              let shade = map(n, 0, points, 0, 10)
+              line(plotX + shade, plotY + shade, plotX, plotY)
+            }
+          }
+        }
+        else if (check > this.splitP) {
+          let weight = weightF(inter, 0.15);
+          let displacement = displaceF(n, this.displacement);
+          vertex(plotX + displacement / 2, plotY + displacement / 2)
+          ellipse(
+            plotX + displacement / 2,
+            plotY + displacement / 2,
+            weight,
+            weight
+          );
+          // rect(
+          //     plotX + displacement / 2,
+          //     plotY + displacement / 2,
+          //     3,
+          //     1
+          //   );
+          if(this.shade && n%12 ==0){
+            console.log("In Shade")  
+            stroke(c)
+              strokeWeight(weight)
+              // let shade = map(plotY, height/2, 3*height/4, 1, 100, true) + random(-10, 10)
+              // line(plotX + shade, plotY - shade, plotX, plotY)
+              // line(plotX - shade, plotY - shade, plotX, plotY)
+            }
+        }
+        if (bubbleCheck && n == points - 1 && !willSplatter) {
+          let weight = weightF(inter, 0.35);
+          ellipse(plotX, plotY, weight, weight);
+        }
+
+        if(showStops != 0 && n == 0){
+          let weight = weightF(inter, 0.35);
+          ellipse(plotX, plotY, weight, weight);
+          textAlign(CENTER, TOP)
+          if(this.drawFunction.title == 'line' || this.drawFunction.title == 'wave'){
+            if(x1 == x2 && y1 < y2){
+              text("a", plotX, plotY - 20)
+            }
+            
+            else if(y1 == y2 && x2 > x1){
+              text("a", plotX - 20, plotY)
+            }
+            
+            else if(y1 == y2 && x2 < x1){
+              text("a", plotX + 20, plotY)
+            }
+            
+            else{
+              text("a", plotX, plotY + 10)
+            }
+            
+            
+            
+          }
+          else{
+            text("a", plotX, plotY + 10)
+          }
+        }
+
+        if(showStops != 0 && n == points - 1){
+          let weight = weightF(inter, 0.35);
+          ellipse(plotX, plotY, weight, weight);
+          textAlign(CENTER, TOP)
+          if (this.drawFunction.title == 'circle' ){
+            text("b", plotX, plotY + 30)  
+          }
+          else if (this.drawFunction.title == 'spiral'){
+            text("b", plotX, plotY - 25)  
+          }
+          else if(this.drawFunction.title == 'line' || this.drawFunction.title == 'wave'){
+            // console.log("We're in line") 
+            if(x1 == x2){
+               if(y2 < y1){
+                 text("b", plotX, plotY - 20) 
+               }
+              
+               else{
+                 text("b", plotX, plotY + 10) 
+               }
+             }
+            
+            else if( y1 == y2){
+              if(x1>x2){
+                text("b", plotX - 20, plotY) 
+              }
+              else{
+                text("b", plotX + 20, plotY) 
+              }
+            }
+            
+            else if (y2 > y1){
+              text("b", plotX, plotY - 10) 
+            }
+          }
+          else{
+            text("b", plotX, plotY + 10)  
+          }
+          
+          // noLoop();
+        }
+      }
+    }
+    endShape();
+  }
+  
+}
+
+
--- sketch.js
+++ sketch.js
@@ -1,9 +1,295 @@
+let inkLine;
+let colors = [];
+let lines = [];
+let totalLines = 16;
+let lineInterval = 0;
+let layers = 0;
+let offset = [];
+let mic;
+let audioContext, audioBuffer;
+let clickableAreas = [];
+let watercolorLayer = null;
+
+let bgColor = [246, 244, 243]
+let levels = [];
+let points = 140;
+let weight = 140;
+
+let currentLine = null;
+let showBuffer = true;
+let inc = 40;
+let allHeartPoints = [];
+let allLinePoints = []
+
+let palettes = [
+  [
+    [47, 72, 88],
+    [214, 40, 57],
+  ],
+  [
+    [55, 63, 81],
+    [0, 141, 213],
+  ],
+  [
+    [229, 99, 153],
+    [50, 14, 59],
+  ],
+  [
+    [252, 186, 4],
+    [165, 1, 4],
+  ],
+  [
+    [165, 1, 4],
+    [100, 141, 229],
+  ],
+  [
+    [80, 114, 85],
+    [72, 139, 73],
+  ],
+];
+
+let sourceSerif = null;
+
+let audioBuffers = [];
+
+let audioFiles = []
+
+let languages = [
+  {"Language": "Irish", "Translation": "Ta grá agam duit"},
+  {"Language": "Tagalog", "Translation": "I love you"},  
+  {"Language": "Farsi", "Translation": "I love you"},  
+  {"Language": "English", "Translation": "I love you"},
+    {"Language": "Spanish", "Translation": "te quiero"},
+    {"Language": "Portuguese", "Translation": "Eu te amo"},
+    {"Language": "Cantonese", "Translation": "我愛你"},
+    {"Language": "French", "Translation": "Je t’aime"},
+    {"Language": "Mandarin", "Translation": "我爱你"},
+    {"Language": "Punjabi", "Translation": "ਮੈਂ ਤੁਹਾਨੂੰ ਪਿਆਰ ਕਰਦਾ ਹਾਂ"},
+    {"Language": "Vietnamese", "Translation": "Em thương anh"},
+    {"Language": "Greek", "Translation": "Σ' αγαπώ"},
+    {"Language": "Telugu", "Translation": "నేను నిన్ను ప్రేమిస్తున్నాను"},
+    {"Language": "Hebrew", "Translation": "אני אוהבת אותך"},
+    {"Language": "German", "Translation": "Ich liebe dich"},    
+    {"Language": "Korean", "Translation": "사랑해"},
+    {"Language": "Japanese", "Translation": "だいすきだよ"},
+    {"Language": "Ukrainian", "Translation": "Я тебе кохаю"},
+    {"Language": "Malay", "Translation": "Saya cinta padamu"}, 
+    {"Language": "Arabic", "Translation": "أحبك"},
+    {"Language": "Hindi", "Translation": "أحبك"},
+    {"Language": "Swedish", "Translation": "Jag älskar dig"},
+    {"Language": "Dutch", "Translation": "Ik hou van jou"},
+  
+//     Need to add in audio files
+    // {"Language": "Italian", "Translation": "Ti amo"},
+    
+    
+//     {"Language": "Swahili", "Translation": "Nakupenda"},
+    
+]
+
+
+
+// Load everything here
+
+function preload() {
+  for(i = 0; i < languages.length; i++){
+    audioContext = new AudioContext();
+    fetch("./" + languages[i].Language + ".mp3")
+      .then((response) => response.arrayBuffer())
+      .then((arrayBuffer) => audioContext.decodeAudioData(arrayBuffer))
+      .then((buffer) => {
+        audioBuffer = buffer;
+        audioBuffers.push(audioBuffer)
+      })
+      .catch((e) => console.error(e));
+    let sound = loadSound("./" + languages[i].Language + ".mp3");
+    audioFiles.push(sound);
+  }
+}
+
 function setup() {
-    createCanvas(400, 400);
+  shuffleArray(languages);
+  frameRate(60);
+  pixelDensity(1);
+  createCanvas(windowWidth, windowHeight);
+  watercolorLayer = createGraphics(1200, 800);
+    // colors = [[100, 100, 0], [200, 100, 0]]
+  shuffleArray(palettes);
+  // customShuffle(shapeFunctions)
+  // shuffle(shape)
+  totalLines = languages.length;
+  for (let i = 0; i <= totalLines; i++) {
+    let ink = new InkLine(palettes[i % palettes.length], null);
+    ink.setSplatter(0.98 - 1 / 100, 0.4 + 1 / 100, 1);
+    ink.setEndBubble(0.0);
+    ink.setAnalogueness(0.2, 1);
+    ink.setStops(0);
+    lines.push(ink);
   }
-  
-  function draw() {
-    background(220);
-    ellipse(width / 2, height / 2, 50, 50);
+  background(bgColor[0], bgColor[1], bgColor[2]);
+  currentLine = lines[0];
+  textFont('Noto Sans');
+}
+
+
+
+function heartShape(t) {
+  // Heart shape formula
+  let x = 16 * pow(sin(t), 3);
+  let y = -(13 * cos(t) - 5 * cos(2 * t) - 2 * cos(3 * t) - cos(4 * t));
+  return { position: createVector(x, y) };
+}
+
+
+
+function shuffleArray(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1)); // Random index from 0 to i
+    [array[i], array[j]] = [array[j], array[i]]; // Swap elements
   }
+  return array;
+}
+
+function draw() {
+  if (showBuffer && audioBuffers.length > 1) {
+    let scale = 5; // Adjust scale to fit the heart shape in your canvas
+    let rows = 3
+    let cols = 8
+    
+    let gridWidth = width / cols; // Divide the canvas width into 5 columns
+    let gridHeight = height / rows; // Divide the canvas height into 2 rows
+    angleMode(RADIANS)
+    
+    for (let gridY = 0; gridY < rows; gridY++) {
+      for (let gridX = 0; gridX < cols; gridX++) {
+      
+        let bufferLength = audioBuffers[(gridX+gridY)%audioBuffers.length].length;
+        let data = audioBuffers[((gridX+gridY)%audioBuffers.length)].getChannelData(0);
+        
+        let offsetX = gridWidth * gridX + gridWidth / 2;
+        let offsetY = gridHeight * gridY + gridHeight / 2;
+        
+        let areaX = gridWidth * gridX
+        let areaY = gridHeight * gridY 
+        clickableAreas.push({ x: areaX, y: areaY, width: gridWidth, height: gridHeight, index: gridX+(8*gridY) });
+        
+        let linePoints = [];
+        let heartLinePoints = []; // Array for this heart's points
+        for (let i = 0; i < bufferLength; i += inc) {
+          let finalX, finalY;
+          
+          let amplitude = map(data[i], -1, 1, -200, 200);
+
+          finalX = offsetX - gridWidth/2 + map(i, 0, bufferLength, 10, gridWidth-10)
+          finalY = offsetY + gridHeight/32 + amplitude
+          if(i%inc == 0){
+            linePoints.push({ x: finalX, y: finalY });
+          }
+          
+          let scalar = 5;
+          
+          let t = map(i, 0, bufferLength, 0, TWO_PI); //
+          
+          // Map buffer index to angle
+          let heartPoint = heartShape(t);
+          // Calculate the displaced point
+          let normalX = heartPoint.position.x
+          let normalY = heartPoint.position.y
+
+          finalX = normalX * scalar + offsetX;
+          finalY = normalY * scalar + offsetY;
+          heartLinePoints.push({ x: finalX, y: finalY });
+        }
+    
+        allHeartPoints.push(heartLinePoints);
+        allLinePoints.push(linePoints);
+        
+        // Add this heart's points to the main array
+
+        // Draw text box under each heart
+        fill(lines[(gridX+(cols*gridY))%(languages.length)].colors[0]);
+        stroke(0)
+        strokeWeight(1)
+        textSize(18);
+        textAlign(CENTER, BOTTOM);
+        text(languages[(gridX+(cols*gridY))%(languages.length)].Translation, offsetX, offsetY + 75);
+        textSize(12);
+        textAlign(CENTER, TOP);
+        text(languages[(gridX+(cols*gridY))%(languages.length)].Language, offsetX, offsetY + 75);
+      }
+    }
+
+    showBuffer = false;
+  }
+
+  // Draw each heart using its respective points
+  for (let i = 0; i < allHeartPoints.length; i++) {
+    currentLine = lines[i];
+    currentLine.setPoints(allHeartPoints[i])
+    currentLine.setAnalogueness(0.1, 20);
+    currentLine.animateLine(null, null, null, null, frames*inc, (frames+1)*inc)
+    currentLine.setPoints(allLinePoints[i])
+    currentLine.setAnalogueness(0.1, 1);
+    currentLine.animateLine(null, null, null, null, frames*inc, (frames+1)*inc)
+  }
+  frames++
+  if (redrawVisual){
+    redrawVisualForArea(redrawVisualArea, redrawVisualIndex);
+  }
+}
+
+function scaledHeartShape(t, scale, offsetX, offsetY) {
+  let v = heartShape(t);
+  v.mult(scale); // Scale the heart shape
+  v.add(offsetX, offsetY); // Translate the heart shape to desired position
+  return v;
+}
+
+
+let frames = 0;
+let drawHeart = true;
+let redrawVisual = false;
+let redrawVisualIndex = 0;
+let redrawVisualFrames = 0;
+let redrawVisualArea = null;
+
+function mouseClicked() {  
+    for (let area of clickableAreas) {
+    if (mouseX > area.x && mouseX < area.x + area.width &&
+        mouseY > area.y && mouseY < area.y + area.height) {
+      // Play the corresponding audio file
+      fill(bgColor[0], bgColor[1], bgColor[2])
+      noStroke()
+      rect(area.x, area.y, area.width, area.height);
+      audioFiles[area.index].play();
+      redrawVisual = true;
+      redrawVisualIndex = area.index;
+      
+      redrawVisualArea = area;
+      // console.log(redrawVisualIndex)
+      redrawVisualFrames = 0;
+      
+      break; // Stop checking further if one match is found
+    }
+  }
+}
+
+function redrawVisualForArea(area, index){
+    currentLine = lines[index];
+      currentLine.setPoints(allHeartPoints[i])
+    currentLine.setPoints(allHeartPoints[index])    
+    currentLine.animateLine(null, null, null, null, redrawVisualFrames*inc, (redrawVisualFrames+1)*inc)
+    currentLine.setPoints(allLinePoints[index])
+    currentLine.animateLine(null, null, null, null, redrawVisualFrames*inc, (redrawVisualFrames+1)*inc)
+  redrawVisualFrames++;
   
+  if((redrawVisualFrames*inc) >= allLinePoints[index].length){    
+    redrawVisual = false;    
+  }
+}
+
+function keyPressed() {
+  if (key === "s") {
+    saveCanvas("lines.png");
+  }
+}
